5f613ae51f2a1bff8990915a4c52bd732c0ba886
tingjoybits <66327730+tingjoybits@users.noreply.github.com>
2020-11-23 18:32:30 +0200

    change wiki url

--- Mesh_Onion_Skins.py
+++ Mesh_Onion_Skins.py
@@ -34,7 +34,7 @@ bl_info = {
     'blender': (2, 80, 0),
     'location': "View3D > Animation > Mesh Onion Skins",
     'description': "Mesh Onion Skins for Blender Animations",
-    'wiki_url': "https://gumroad.com/l/OqkKG",
+    'wiki_url': "https://github.com/tingjoybits/Mesh_Onion_Skins",
     'category': "Animation"}
 
 
